48e6e763958c518efdf95494aff8e01abb5de391
Daniel Eduardo Flores Hernández <daefher@gmail.com>
2018-05-10 01:13:25 -0500

    Version Normal

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ def main():
     Lista = [6,5,3,1,8,7,2,4]
     ord = Insercion(Lista)
     print("Lista desordenada \t", Lista)
-    #print("Lista ordenada    \t", ord.insercion())
-    print("Lista ordenadaRec \t", ord.insercionRec(0))
+    print("Lista ordenada    \t", ord.insercion())
+    #print("Lista ordenadaRec \t", ord.insercionRec(0))
 
 
 if __name__ == main():
